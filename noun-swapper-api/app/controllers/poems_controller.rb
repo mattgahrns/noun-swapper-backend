98a1c8a3d1bba3a8375ec8674a497ca90f86b9f4
Matt Gahrns <matthewgahrns@hotmail.com>
2019-12-03 14:27:24 -0800

    Backend functionality for reading and creating poems

--- noun-swapper-api/app/controllers/poems_controller.rb
+++ noun-swapper-api/app/controllers/poems_controller.rb
@@ -1,2 +1,19 @@
 class PoemsController < ApplicationController
+    def index
+        poems = Poem.all
+        render json: poems
+    end
+
+    def create
+        poem = Poem.new(poem_params)
+        poem.user_id = User.first.id
+        if poem.save
+            render json: poem
+        end
+    end
+
+    private
+    def poem_params
+        params.require(:poem).permit(:content, :modified_content)
+    end
 end
--- noun-swapper-api/db/seeds.rb
+++ noun-swapper-api/db/seeds.rb
@@ -6,3 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create(username: "Test")
+
+jen = User.create(username: "Jen")
+
+Poem.create(title: "Test Poem", content: "This is a poem. It is a special poem, because it is for a test.", modified_content: "This is a dog. It is a special cat, because it is for a poem.", user_id: 1)
+
+Poem.create(title: "I Love Anime", content: "I love anime, it is great and full of awesome characters.", modified_content: "I love food, it is great and full of awesome buzzwords", user_id: jen.id)
